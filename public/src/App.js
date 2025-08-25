f30103983c02af3a39deb1e18a7b0e917d56c458
ubongudo649-netizen <ubongudo649@gmail.com>
2025-08-25 12:18:19 +0100

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Digital Store</h1>
+      <p>Here you can buy ebooks, guides, and art 🎨</p>
+    </div>
+  );
+}
+
+export default App;
